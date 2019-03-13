bac405e1257b99ad7c32ad7bb92397a9121366b1
tonghao <tong-hao@hotmail.com>
2019-03-13 22:56:13 +0800

    Init helloWorld

---
+++ src/main/java/edu/nsu/cloud/Greeting.java
@@ -0,0 +1,23 @@
+package edu.nsu.cloud;
+
+public class Greeting {
+
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    private final long id;
+    private final String content;
+
+
+}
